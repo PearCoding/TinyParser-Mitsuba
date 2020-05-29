df25b4bbe4485ede4e21883d21711c684db89667
PearCoding <pearcoding@gmail.com>
2020-05-29 14:55:54 +0200

    Added properties

--- include/tinyparser-mitsuba.h
+++ include/tinyparser-mitsuba.h
@@ -23,7 +23,30 @@
 
 #define TPM_NAMESPACE tinyparser_mitsuba
 
+// Check for C++ features
+/*#ifdef __has_cpp_attribute
+#if __has_cpp_attribute(nodiscard)
+#define TPM_NODISCARD [[nodiscard]]
+#endif
+#endif*/
+
+#ifndef TPM_NODISCARD
+#if __cpp_constexpr >= 201603L
+#define TPM_NODISCARD [[nodiscard]]
+#else
+#define TPM_NODISCARD
+#endif
+#endif
+
 namespace TPM_NAMESPACE {
+#ifdef TPM_NUMBER_AS_DOUBLE
+using Number = double;
+#else
+using Number = float;
+#endif
+
+using Integer = int64_t;
+
 // --------------- Enums
 enum EntityType {
 	ET_BSDF = 0,
@@ -39,50 +62,279 @@ enum EntityType {
 
 enum PropertyType {
 	PT_INTEGER = 0,
-	PT_FLOAT,
+	PT_NUMBER,
 	PT_BOOL,
 	PT_STRING,
 	PT_POINT,
 	PT_VECTOR,
+	PT_TRANSFORM,
 	PT_RGB,
 	PT_SPECTRUM
 };
 
 // --------------- Vector/Points
 struct TPM_LIB Vector {
-	float x, y, z;
-	inline Vector(float x, float y, float z)
+	Number x, y, z;
+	inline Vector(Number x, Number y, Number z)
 		: x(x)
 		, y(y)
 		, z(z)
 	{
 	}
 };
+inline TPM_NODISCARD bool operator==(const Vector& a, const Vector& b)
+{
+	return a.x == b.x && a.y == b.y && a.z == b.z;
+}
+inline TPM_NODISCARD bool operator!=(const Vector& a, const Vector& b)
+{
+	return !(a == b);
+}
 using Point = Vector;
 
 // --------------- Transform
 class TPM_LIB Transform {
 };
 
+// --------------- RGB
+struct TPM_LIB RGB {
+	Number r, g, b;
+	inline RGB(Number r, Number g, Number b)
+		: r(r)
+		, g(g)
+		, b(b)
+	{
+	}
+};
+inline TPM_NODISCARD bool operator==(const RGB& a, const RGB& b)
+{
+	return a.r == b.r && a.g == b.g && a.b == b.b;
+}
+inline TPM_NODISCARD bool operator!=(const RGB& a, const RGB& b)
+{
+	return !(a == b);
+}
+
 // --------------- Spectrum
 class TPM_LIB Spectrum {
 public:
 	Spectrum() = default;
-	inline Spectrum(const std::vector<int>& wavelengths, const std::vector<float>& weights)
+	inline Spectrum(const std::vector<int>& wavelengths, const std::vector<Number>& weights)
 		: mWavelengths(wavelengths)
 		, mWeights(weights)
 	{
 	}
 
-	inline const std::vector<int>& wavelengths() const { return mWavelengths; }
-	inline const std::vector<float>& weights() const { return mWeights; }
+	inline TPM_NODISCARD const std::vector<int>& wavelengths() const { return mWavelengths; }
+	inline TPM_NODISCARD const std::vector<Number>& weights() const { return mWeights; }
 
 private:
 	std::vector<int> mWavelengths;
-	std::vector<float> mWeights;
+	std::vector<Number> mWeights;
 };
 
+// --------------- Property
 class TPM_LIB Property {
 public:
+	Property(const Property& other) = default;
+	Property(Property&& other)		= default;
+
+	Property& operator=(const Property& other) = default;
+	Property& operator=(Property&& other) = default;
+
+	inline TPM_NODISCARD PropertyType type() const { return mType; }
+
+	inline Number getNumber(Number def = Number(0), bool* ok = nullptr) const
+	{
+		if (mType == PT_NUMBER) {
+			if (ok)
+				*ok = true;
+			return mNumber;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromNumber(Number v)
+	{
+		Property p(PT_NUMBER);
+		p.mNumber = v;
+		return p;
+	}
+
+	inline Integer getInteger(Integer def = Integer(0), bool* ok = nullptr) const
+	{
+		if (mType == PT_INTEGER) {
+			if (ok)
+				*ok = true;
+			return mInteger;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromInteger(Integer v)
+	{
+		Property p(PT_INTEGER);
+		p.mInteger = v;
+		return p;
+	}
+
+	inline bool getBool(bool def = false, bool* ok = nullptr) const
+	{
+		if (mType == PT_BOOL) {
+			if (ok)
+				*ok = true;
+			return mBool;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromBool(bool b)
+	{
+		Property p(PT_BOOL);
+		p.mBool = b;
+		return p;
+	}
+
+	inline const Point& getPoint(const Point& def = Point(0, 0, 0), bool* ok = nullptr) const
+	{
+		if (mType == PT_POINT) {
+			if (ok)
+				*ok = true;
+			return mPoint;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromPoint(const Point& v)
+	{
+		Property p(PT_POINT);
+		p.mPoint = v;
+		return p;
+	}
+
+	inline const Vector& getVector(const Vector& def = Vector(0, 0, 0), bool* ok = nullptr) const
+	{
+		if (mType == PT_VECTOR) {
+			if (ok)
+				*ok = true;
+			return mVector;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromVector(const Vector& v)
+	{
+		Property p(PT_VECTOR);
+		p.mVector = v;
+		return p;
+	}
+
+	inline const Transform& getTransform(const Transform& def = Transform(), bool* ok = nullptr) const
+	{
+		if (mType == PT_TRANSFORM) {
+			if (ok)
+				*ok = true;
+			return mTransform;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromTransform(const Transform& v)
+	{
+		Property p(PT_TRANSFORM);
+		p.mTransform = v;
+		return p;
+	}
+
+	inline const RGB& getRGB(const RGB& def = RGB(0, 0, 0), bool* ok = nullptr) const
+	{
+		if (mType == PT_RGB) {
+			if (ok)
+				*ok = true;
+			return mRGB;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromRGB(const RGB& rgb)
+	{
+		Property p(PT_RGB);
+		p.mRGB = rgb;
+		return p;
+	}
+
+	inline const std::string& getString(const std::string& def = "", bool* ok = nullptr) const
+	{
+		if (mType == PT_STRING) {
+			if (ok)
+				*ok = true;
+			return mString;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromString(const std::string& v)
+	{
+		Property p(PT_STRING);
+		p.mString = v;
+		return p;
+	}
+
+	inline const Spectrum& getSpectrum(const Spectrum& def = Spectrum(), bool* ok = nullptr) const
+	{
+		if (mType == PT_SPECTRUM) {
+			if (ok)
+				*ok = true;
+			return mSpectrum;
+		} else {
+			if (ok)
+				*ok = false;
+			return def;
+		}
+	}
+	static TPM_NODISCARD inline Property fromSpectrum(const Spectrum& spec)
+	{
+		Property p(PT_SPECTRUM);
+		p.mSpectrum = spec;
+		return p;
+	}
+
+private:
+	inline Property(PropertyType type)
+		: mType(type)
+	{
+	}
+
+	PropertyType mType;
+
+	// Data Types
+	union {
+		Number mNumber;
+		Integer mInteger;
+		bool mBool;
+
+		Point mPoint;
+		Vector mVector;
+		Transform mTransform;
+		RGB mRGB;
+	};
+	std::string mString;
+	Spectrum mSpectrum;
 };
 } // namespace TPM_NAMESPACE
---
+++ src/tests/property.cpp
@@ -0,0 +1,32 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "tinyparser-mitsuba.h"
+
+using namespace TPM_NAMESPACE;
+
+TEST_CASE("Property Construction", "[property]")
+{
+	REQUIRE(Property::fromNumber(Number(0)).type() == PT_NUMBER);
+	REQUIRE(Property::fromInteger(Integer(0)).type() == PT_INTEGER);
+	REQUIRE(Property::fromBool(false).type() == PT_BOOL);
+	REQUIRE(Property::fromPoint(Point(0, 0, 0)).type() == PT_POINT);
+	REQUIRE(Property::fromVector(Vector(0, 0, 0)).type() == PT_VECTOR);
+	REQUIRE(Property::fromRGB(RGB(0, 0, 0)).type() == PT_RGB);
+	REQUIRE(Property::fromTransform(Transform()).type() == PT_TRANSFORM);
+	REQUIRE(Property::fromString("").type() == PT_STRING);
+	REQUIRE(Property::fromSpectrum(Spectrum()).type() == PT_SPECTRUM);
+}
+
+TEST_CASE("Property Access", "[property]")
+{
+	REQUIRE(Property::fromNumber(Number(1)).getNumber() == Number(1));
+	REQUIRE(Property::fromInteger(Integer(1)).getInteger() == Integer(1));
+	REQUIRE(Property::fromBool(true).getBool() == true);
+	REQUIRE(Property::fromPoint(Point(1, 0, 0)).getPoint() == Point(1, 0, 0));
+	REQUIRE(Property::fromVector(Vector(1, 0, 0)).getVector() == Vector(1, 0, 0));
+	REQUIRE(Property::fromRGB(RGB(1, 0, 0)).getRGB() == RGB(1, 0, 0));
+	//REQUIRE(Property::fromTransform(Transform()).getTransform() == Transform());
+	REQUIRE(Property::fromString("TEST").getString() == "TEST");
+	//REQUIRE(Property::fromSpectrum(Spectrum()).getSpectrum() == Spectrum());
+}

aecc6e7298f11e4e453c2f9d76f0ff48ca6e5c5e
Ömercan Yazici <pearcoding@gmail.com>
2021-07-23 21:52:06 +0200

    Fixed some nuget stuff

--- cmake/setup_install.cmake
+++ cmake/setup_install.cmake
@@ -37,5 +37,15 @@ install(EXPORT ${TPM_TARGET}Targets NAMESPACE ${TPM_NAMESPACE}::
 		COMPONENT ${TPM_TARGET}_config)
 
 if(BUILD_CLI_WRAPPER)
-    install(TARGETS ${TPM_TARGET}_cli DESTINATION . COMPONENT ${TPM_TARGET}_managed)
+    if(WIN32)
+        set(PLATFORM "win10")
+    elseif(APPLE)
+        set(PLATFORM "macos")
+    else()
+        set(PLATFORM "linux")
+    endif()
+    install(TARGETS ${TPM_TARGET}_cli
+        COMPONENT ${TPM_TARGET}_managed
+        RUNTIME DESTINATION "runtimes/${PLATFORM}-x64/lib/netcoreapp3.1"
+        LIBRARY DESTINATION "lib")
 endif()
--- cmake/setup_package.cmake
+++ cmake/setup_package.cmake
@@ -2,6 +2,7 @@ set(CPACK_PACKAGE_VENDOR "PearCoding")
 set(CPACK_PACKAGE_DESCRIPTION "Tiny parser for scene files defined by the Mitsuba renderer")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Tiny parser for scene files defined by the Mitsuba renderer")
 set(CPACK_PACKAGE_CONTACT "PearCoding <omercan AT pearcoding DOT eu>")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/PearCoding/TinyParser-Mitsuba")
 set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
@@ -30,6 +31,9 @@ set(CPACK_RPM_PACKAGE_LICENSE "MIT")
 set(CPACK_RPM_MAIN_COMPONENT ${TPM_TARGET}_runtime)
 
 if(BUILD_CLI_WRAPPER)
+    set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR};${PROJECT_NAME};${TPM_TARGET}_managed;/")
+    set(CPACK_NUGET_COMPONENT_INSTALL ON)
+    set(CPACK_NUGET_PACKAGE_NAME "TinyParserMitsuba")
     set(CPACK_NUGET_PACKAGE_LICENSE_EXPRESSION "MIT")
     set(CPACK_NUGET_PACKAGE_TAGS "Parser Mitsuba Rendering Raytracer SceneDescription Wrapper")
     set(CPACK_GENERATOR ${CPACK_GENERATOR} NuGet)
@@ -40,4 +44,4 @@ cpack_add_install_type(${TPM_TARGET}_managed
                     DISPLAY_NAME "TinyParser-Mitsuba CLI Wrapper")
 cpack_add_component(${TPM_TARGET}_managed
                     DISPLAY_NAME "TinyParser-Mitsuba CLI Wrapper"
-                    INSTALL_TYPES Managed)
+                    INSTALL_TYPES ${TPM_TARGET}_managed)
